324827d88fbfbdc0fefdd8acd688f2c4631e3a1a
pujangga123 <rush_x@yahoo.com>
2021-04-19 16:32:41 +0700

    add mail.banyak

--- assets/mail.php
+++ assets/mail.php
@@ -1,8 +1,9 @@
 <?php
     $nama = $_POST['nama'];
     $hp = $_POST['hp'];
+    $banyak = $_POST['banyak'];
 
-    if(mail("pujangga123@gmail.com","KEHADIRAN:".$nama,"$nama,$hp")) {
+    if(mail("pujangga123@gmail.com","KEHADIRAN:".$nama,"$nama,$hp,$banyak")) {
         die("ok");
     } else {
         die("no");
